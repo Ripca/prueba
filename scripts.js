a95c12f587447caf6ecb801c069013b958371ce4
Ripca <117389221+Ripca@users.noreply.github.com>
2023-11-18 10:07:15 -0600

    terminado

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 const barra=document.getElementById("barra");
+const barra2=document.getElementById("barra2");
 
 const rootStyles=document.documentElement.style;
 
 barra.addEventListener("click", (e)=>{
     rootStyles.setProperty('--transparenc-e',`${e.target.value}%`)
+    
+})
+barra2.addEventListener("click", (e)=>{
+    
+    rootStyles.setProperty('--size',`${e.target.value}px`)
 })
